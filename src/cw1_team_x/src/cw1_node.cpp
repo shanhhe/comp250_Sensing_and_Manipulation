0dd721d59038c146e9c4c0be59f24bc606faa5c4
EddiePJEdwards <eddie@HP-5CD422HTLM.cs.ucl.ac.uk>
2025-02-07 14:26:51 +0000

    Added CW1

---
+++ src/cw1_team_x/src/cw1_node.cpp
@@ -0,0 +1,23 @@
+/* feel free to change any part of this file, or delete this file. In general,
+you can do whatever you want with this template code, including deleting it all
+and starting from scratch. The only requirment is to make sure your entire 
+solution is contained within the cw1_team_<your_team_number> package */
+
+#include <cw1_class.h>
+
+int main(int argc, char **argv){
+  
+  ros::init(argc,argv, "cw1_solution_node");
+  ros::NodeHandle nh;
+
+  // create an instance of the cw1 class
+  cw1 cw_class(nh);
+
+  ros::Rate loop_rate(10);
+
+  while (ros::ok()){
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
